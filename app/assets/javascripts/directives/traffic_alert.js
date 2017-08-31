711e2d6fac82686b16137fc037627a3d2b3c5d22
Paul Wenig <pwenig@gmail.com>
2017-08-31 08:55:01 -0600

    Initial traffic alerts

---
+++ app/assets/javascripts/directives/traffic_alert.js
@@ -0,0 +1,16 @@
+app.directive('travelalert', function() {
+  
+  return {
+    restrict: 'E',
+    templateUrl: 'travel_alert.html',
+    link: function ($scope, element, attributes) {
+
+      console.log('DIRECTIVE')
+
+  
+    }
+
+  
+  }
+    
+});
